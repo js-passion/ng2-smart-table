2280ae45eb622ae1e0cf553f638af44c5e4fac86
AMAIISDOM\singhm <mayanksingh0821@gmail.com>
2017-09-15 11:16:01 +0530

    removing build errors

--- src/ng2-smart-table/lib/validator.service.ts
+++ src/ng2-smart-table/lib/validator.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export abstract class ValidatorService {
-     notify = new Subject<any>();
+    notify = new Subject<any>();
     notifyObservable$ = this.notify.asObservable();
     abstract getFormGroup(): FormGroup;
-    abstract notifyOther(data: any); 
+    abstract notifyOther(data: any) : boolean; 
 }
